5db4e97c589f38fe6ec9312de93bf7ee020ca374
thoppenbrouwer <thoppenbrouwer@gmail.com>
2019-03-19 16:34:55 +0100

    added paparazzo amices

---
+++ Paparazzo/sw/airborne/modules/orange_avoider/guido_marinara.h
@@ -0,0 +1,15 @@
+/*
+ * guido_marinara.h
+ *
+ *  Created on: 14 mrt. 2019
+ *      Author: tomhoppenbrouwer
+ */
+
+#ifndef SW_AIRBORNE_MODULES_ORANGE_AVOIDER_GUIDO_MARINARA_H_
+#define SW_AIRBORNE_MODULES_ORANGE_AVOIDER_GUIDO_MARINARA_H_
+
+
+
+
+
+#endif /* SW_AIRBORNE_MODULES_ORANGE_AVOIDER_GUIDO_MARINARA_H_ */
